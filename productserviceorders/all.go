321673c415256e3f95ec70bcb545b19f105a54ae
Paul Jacobse <paul.jacobse@omniboost.com>
2026-04-28 10:13:32 +0200

    feat: [IN-4182] add LinkedReservationId on ProductServiceOrder

--- productserviceorders/all.go
+++ productserviceorders/all.go
@@ -36,7 +36,7 @@ func (s *APIService) All(requestBody *AllRequest) (*AllResponse, error) {
 
 type AllResponse struct {
 	ProductServiceOrders ProductServiceOrders
-	Cursor    string    `json:"Cursor"`
+	Cursor               string `json:"Cursor"`
 }
 
 func (s *APIService) NewAllRequest() *AllRequest {
@@ -60,25 +60,26 @@ func (r AllRequest) MarshalJSON() ([]byte, error) {
 type ProductServiceOrders []ProductServiceOrder
 
 type ProductServiceOrder struct {
-	ID                string              `json:"Id"`                // Unique identifier of the product service order.
-	ServiceID         string              `json:"ServiceId"`         // Unique identifier of the Service that the product service order is made against.
-	AccountID         string              `json:"AccountId"`         // Unique identifier of the Customer or a Company who owns the product service order.
-	AccountType       string              `json:"AccountType"`       // A discriminator specifying the type of the Account, e.g. customer or company.
-	CreatorProfileID  string              `json:"CreatorProfileId"`  // Unique identifier of the user who created the order item.
-	UpdateProfileID   string              `json:"UpdateProfileId"`   // Unique identifier of the user who updated the order item.
-	StartUTC          time.Time           `json:"StartUtc"`          // Product service order start in UTC timezone in ISO 8601 format.
-	EndUTC            time.Time           `json:"EndUtc"`            // Product service order end in UTC timezone in ISO 8601 format.
-	BookerID          string              `json:"BookerId"`          // Unique identifier of the customer on whose behalf the product service order was made.
-	Number            string              `json:"Number"`            // Confirmation number of the service order in Mews.
-	State             ServiceOrderState   `json:"State"`             // State of the product service order.
-	Origin            ServiceOrderOrigin  `json:"Origin"`            // Origin of the product service order.
-	OriginDetails     string              `json:"OriginDetails"`     // Details about the product service order origin.
-	CreatedUTC        time.Time           `json:"CreatedUtc"`        // Creation date and time of the product service order in UTC timezone in ISO 8601 format.
-	UpdatedUTC        time.Time           `json:"UpdatedUtc"`        // Last update date and time of the product service order in UTC timezone in ISO 8601 format.
-	CancelledUTC      time.Time           `json:"CancelledUtc"`      // Cancellation date and time of the product service order in UTC timezone in ISO 8601 format.
-	VoucherID         string              `json:"VoucherId"`         // Unique identifier of the voucher that has been used to create the product service order.
-	BusinessSegmentID string              `json:"BusinessSegmentId"` // Unique identifier of the product service order Business segment.
-	Options           ServiceOrderOptions `json:"Options"`           // Options of the service order.
+	ID                  string              `json:"Id"`                   // Unique identifier of the product service order.
+	ServiceID           string              `json:"ServiceId"`            // Unique identifier of the Service that the product service order is made against.
+	AccountID           string              `json:"AccountId"`            // Unique identifier of the Customer or a Company who owns the product service order.
+	AccountType         string              `json:"AccountType"`          // A discriminator specifying the type of the Account, e.g. customer or company.
+	CreatorProfileID    string              `json:"CreatorProfileId"`     // Unique identifier of the user who created the order item.
+	UpdateProfileID     string              `json:"UpdateProfileId"`      // Unique identifier of the user who updated the order item.
+	StartUTC            time.Time           `json:"StartUtc"`             // Product service order start in UTC timezone in ISO 8601 format.
+	EndUTC              time.Time           `json:"EndUtc"`               // Product service order end in UTC timezone in ISO 8601 format.
+	BookerID            string              `json:"BookerId"`             // Unique identifier of the customer on whose behalf the product service order was made.
+	Number              string              `json:"Number"`               // Confirmation number of the service order in Mews.
+	State               ServiceOrderState   `json:"State"`                // State of the product service order.
+	Origin              ServiceOrderOrigin  `json:"Origin"`               // Origin of the product service order.
+	OriginDetails       string              `json:"OriginDetails"`        // Details about the product service order origin.
+	CreatedUTC          time.Time           `json:"CreatedUtc"`           // Creation date and time of the product service order in UTC timezone in ISO 8601 format.
+	UpdatedUTC          time.Time           `json:"UpdatedUtc"`           // Last update date and time of the product service order in UTC timezone in ISO 8601 format.
+	CancelledUTC        time.Time           `json:"CancelledUtc"`         // Cancellation date and time of the product service order in UTC timezone in ISO 8601 format.
+	VoucherID           string              `json:"VoucherId"`            // Unique identifier of the voucher that has been used to create the product service order.
+	BusinessSegmentID   string              `json:"BusinessSegmentId"`    // Unique identifier of the product service order Business segment.
+	LinkedReservationId string              `json:"LinkedReservationIds"` // Identifier of the linked Reservation.
+	Options             ServiceOrderOptions `json:"Options"`              // Options of the service order.
 }
 
 type ServiceOrderOptions struct {
